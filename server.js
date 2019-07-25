a7abac33e2f24e1454c2c699e769a7211ebc9dd5
Erin Johnson <erinjohnson47@gmail.com>
2019-07-25 16:25:29 -0700

    db and server setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const methodOverride = require('method-override');
+const session = require('express-session')
+const app = express();
+const logger = require('morgan');
+
+require('./db/db');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(logger('dev'));
+app.use(session({
+    secret: 'THIS IS A RANDOM SECRET STRING', 
+    resave: false, 
+    saveUninitialized: false
+}));
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+});
