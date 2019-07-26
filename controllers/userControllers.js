b119fd2d1a84e3b817f06d63a3d608a121ecf5c0
Erin Johnson <erinjohnson47@gmail.com>
2019-07-26 11:41:38 -0700

    set up user controller, export

---
+++ controllers/userControllers.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const User = require('../models/User');
+const Meme = require('../models/Meme');
+const bcrypt = require('bcryptjs')
+
+
+
+const userController = {
+
+}
+
+module.exports = userController;
