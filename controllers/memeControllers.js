30f017a1b06b0ae70bce0ffb3bdf94435bbb0c10
Austin <wasuen@gmail.com>
2019-07-26 11:41:55 -0700

    setup meme controller and route directory

---
+++ controllers/memeControllers.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const User = require('../models/User');
+const Meme = require('../models/Meme');
+
+
+const memeController = {
+
+}
+
+module.exports = memeController;
---
+++ routers/memeRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const memeController = require('../controllers/memeControllers');
+
+
+module.exports = router;
