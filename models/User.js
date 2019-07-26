0ae0189e459423177a287672d2bf98cb70e2494d
Erin Johnson <erinjohnson47@gmail.com>
2019-07-26 14:37:48 -0700

    fix user and meme schemas redundancy

--- models/User.js
+++ models/User.js
@@ -2,14 +2,10 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: String,
-    username: {type: String, unique: true, required: true},
+    username: {type: String, unique: true, required: true, lowercase: true},
     password: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
+    email: {type: String, required: true, lowercase: true, unique: true},
     dob: {type: String, required: true},
-    meme: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Meme'
-    }],
     profileImg: String
 })
 
