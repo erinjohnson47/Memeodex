9cd8f4d73f271d1cb412835237e5ff7cb4db7e48
Austin <wasuen@gmail.com>
2019-07-26 13:22:48 -0700

    add meme model

---
+++ models/Meme.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const memeSchema = new mongoose.Schema({
+    urlMeme: {type: String, required: true},
+    title: {type: String, required: true},
+    description: String,
+    date: String,
+    username: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    hashtag: [String]
+})
+
+const Meme = mongoose.model('Meme', memeSchema);
+
+module.exports = Meme;
