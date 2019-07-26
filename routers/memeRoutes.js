07a33d7bdae67ad579bd08703ca050ec2f927122
Austin <wasuen@gmail.com>
2019-07-26 14:46:36 -0700

    add get route to new

--- routers/memeRoutes.js
+++ routers/memeRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const memeController = require('../controllers/memeControllers');
 
 router.get('/', memeController.index);
+router.get('/new', memeController.new);
 
 
 module.exports = router;
