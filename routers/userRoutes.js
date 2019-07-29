4467fa63de8e634df0895c1e47333770f6750a66
Erin Johnson <erinjohnson47@gmail.com>
2019-07-29 14:42:34 -0700

    session middleware to protect routes, add/fix session on edit, register, update

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -2,12 +2,22 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userControllers');
 
+const isLogged = (req, res, next) => {
+    if (req.session.logged) {
+        next()
+    } else {
+        req.session.message = 'You must be logged in';
+        res.redirect('/');
+    }
+}
+
 router.get('/register', userController.register);
 router.post('/login',userController.login);
 router.get('/', userController.index);
 router.post('/', userController.create);
+router.get('/logout', userController.logout)
 router.get('/:id', userController.profile);
-router.get('/:id/edit', userController.edit);
+router.get('/:id/edit', isLogged, userController.edit);
 router.put('/:id', userController.update);
 router.delete('/:id', userController.delete);
 
