86450eb614dc775ccb8ff07182bb9257f65427a1
Erin Johnson <erinjohnson47@gmail.com>
2019-07-31 09:41:08 -0700

    password match, min length validation added

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,19 @@
+console.log('js is working')
 const password = document.getElementById("password")
-const confirm_password = document.getElementById("confirm_password");
+const confirm_password = document.getElementById("password-confirm");
 
-function validatePassword(){
-  if(password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("Passwords Don't Match");
-  } else {
-    confirm_password.setCustomValidity('');
-  }
+const checkPassword = () => {
+    if(password.value != confirm_password.value) {
+        console.log(password.value, '<-password', confirm_password.value, '<-confirm password')
+        confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+        password.setCustomValidity('');
+        confirm_password.setCustomValidity('');
+        console.log(password.value, '<-password', confirm_password.value, '<-confirm password')
+    }
 }
+// password.addEventListener("focusout", checkPassword, true);
+confirm_password.addEventListener("focusout", checkPassword, true);
 
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
+// password.onchange = validatePassword;
+// confirm_password.onkeyup = validatePassword;
