aaf296b9ade882e23326a4de6cab062a13970db0
Erin Johnson <erinjohnson47@gmail.com>
2019-08-01 11:13:31 -0700

    navbar collapses to hamburger icon when screen re-sizes

--- public/app.js
+++ public/app.js
@@ -1,19 +1,35 @@
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("myBtn");
+const modal2 = document.getElementById("myModal2");
+const btn2 = document.getElementById("myBtn2");
 
-var btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("close")[0];
 
-var span = document.getElementsByClassName("close")[0];
+document.addEventListener('DOMContentLoaded', function() {
+  let elems = document.querySelectorAll('.sidenav');
+  let instances = M.Sidenav.init(elems, {});
+});
 
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
 span.onclick = function() {
   modal.style.display = "none";
+}
+span.onclick = function() {
+  modal2.style.display = "none";
 }
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal2.style.display = "none";
   }
 }
