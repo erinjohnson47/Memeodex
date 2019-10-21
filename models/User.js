88641ccf0785931bc3671cfd000b89575420bc8c
Erin Johnson <erinjohnson47@gmail.com>
2019-10-21 10:08:54 -0700

    remove name and dob as requirements for register from model, register and edit show pages

--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: String,
+    // name: String,
     username: {type: String, unique: true, required: true, lowercase: true},
     password: {type: String, required: true},
     email: {type: String, required: true, lowercase: true, unique: true},
-    dob: {type: String, required: true},
+    // dob: {type: String, required: true},
 })
 
 const User = mongoose.model('User', userSchema);
